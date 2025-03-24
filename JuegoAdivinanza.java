2e69bfc9642f8b4b8c32ea2e86762623de03ba9e
grecia68 <grecia777123@gmail.com>
2025-03-23 19:39:10 -0600

    Update JuegoAdivinanza.java

--- JuegoAdivinanza.java
+++ JuegoAdivinanza.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
+
+public static void main (string[] args){
+Scanner scanner = new Scanner (System.in);
+
+System.out.println("Bienvenido al Juego de Adivinar el Numero");
+System.out.println("Tienes" "intentos para adivinar un numero entre 1 y 100");
 
 
 
-
+}
